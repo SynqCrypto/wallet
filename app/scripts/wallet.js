9341e04d3767cc46bca01cb9caaf8015661333dc
Theodor Văraru <theo@vararu.org>
2014-06-30 21:17:28 +0300

    Initial commit. generator-gulp-angular seed.

---
+++ app/scripts/wallet.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('wallet', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
