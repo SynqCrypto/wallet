ed7b5aa44ece0bfd6836889034c079f89f4c5404
Theodor Văraru <theo@vararu.org>
2014-07-02 01:35:01 +0300

    Add basic deposit/withdraw, currency conversion, currency and icon filter.

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -1,12 +1,37 @@
 'use strict';
 
+var RATES = {
+  gbp: 1.00,
+  usd: 0.58,
+  eur: 0.80
+};
+
 angular.module('wallet')
   .controller('WalletCtrl', function ($scope) {
-    $scope.currency = 'eur';
-    $scope.total = 0;
+    $scope.currency = 'gbp';
+    $scope.total = 1;
+
+    $scope.deposit = function () {
+      // Normalize amount when depositing.
+      $scope.total += $scope.depositAmount * RATES[$scope.currency];
+    };
+
+    $scope.withdraw = function () {
+      if ($scope.total - $scope.withdrawAmount < 0) {
+        // Handle this.
+        console.log('Not enough funds.');
+        return;
+      }
+      $scope.total -= $scope.withdrawAmount * RATES[$scope.currency];
+    };
   })
   .filter('icon', function () {
     return function (name) {
       return 'fa-' + name;
+    };
+  })
+  .filter('currency', function () {
+    return function (amount, curr) {
+      return (amount / RATES[curr]).toFixed(2);
     };
   });
