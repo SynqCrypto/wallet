90aee5473e1911d7572d7af7b4d589d40c294039
Theodor Văraru <theo@vararu.org>
2014-07-02 03:53:15 +0300

    Add a couple of unit tests.

--- test/unit/main.js
+++ test/unit/main.js
@@ -1,22 +1,28 @@
 'use strict';
 
-describe('controllers', function(){
-  var scope;
+describe('controllers', function () {
+  var scope, ctrl;
 
   beforeEach(module('wallet'));
 
-  beforeEach(inject(function($rootScope) {
-  	scope = $rootScope.$new();
-  }));
+  describe('WalletCtrl', function () {
+    beforeEach(inject(function ($rootScope, $controller) {
+      scope = $rootScope.$new();
+      ctrl = $controller('WalletCtrl', {
+        $scope: scope
+      });
+    }));
 
-  it('should define more than 5 awesome things', inject(function($controller) {
-    expect(scope.awesomeThings).toBeUndefined();
+    it('should start with 0 transactions', function () {
+      expect(angular.isArray(scope.$storage.transactions)).toBeTruthy();
+      expect(scope.$storage.transactions.length === 0).toBeTruthy();
+    });
 
-    $controller('MainCtrl', {
-      $scope: scope
-  	});
+    it('should be able to deposit', function () {
+      scope.depositAmount = 20;
+      scope.deposit();
 
-    expect(angular.isArray(scope.awesomeThings)).toBeTruthy();
-    expect(scope.awesomeThings.length > 5).toBeTruthy();
-  }));
+      expect(scope.$storage.balance === 20).toBeTruthy();
+    });
+  });
 });
